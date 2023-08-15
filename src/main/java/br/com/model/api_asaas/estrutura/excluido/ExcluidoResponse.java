2303d832761a2108b9a23255eb319053a9e7d7c7
Rafael <rafaelrlcnta@gmail.comm>
2023-08-14 23:36:25 -0300

    chore: mudança de dos POJOS excluir para o pacote estrutura

--- src/main/java/br/com/model/api_asaas/estrutura/excluido/ExcluidoResponse.java
+++ src/main/java/br/com/model/api_asaas/estrutura/excluido/ExcluidoResponse.java
@@ -1,4 +1,4 @@
-package br.com.model.api_asaas.estrutura.auxiliar;
+package br.com.model.api_asaas.estrutura.excluido;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
